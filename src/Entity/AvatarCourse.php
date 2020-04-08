058a97ef32babbec0cf54d556974ade7522c978a
AnnaLaptop <anna.laskowska@hotmail.com>
2020-04-08 10:49:45 +0200

    creation entity  AvatarCourse

---
+++ src/Entity/AvatarCourse.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\AvatarCourseRepository")
+ */
+class AvatarCourse
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $lien;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLien(): ?string
+    {
+        return $this->lien;
+    }
+
+    public function setLien(?string $lien): self
+    {
+        $this->lien = $lien;
+
+        return $this;
+    }
+}
