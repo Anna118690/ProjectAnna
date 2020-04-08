8867a9d42c20b08db39c58900cb2ccfc4d6187bc
AnnaLaptop <anna.laskowska@hotmail.com>
2020-04-08 10:30:17 +0200

    creation entity  LessonOrder

---
+++ src/Entity/LessonOrder.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\LessonOrderRepository")
+ */
+class LessonOrder
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $dateLessonOrder;
+
+    /**
+     * @ORM\Column(type="decimal", precision=6, scale=2)
+     */
+    private $priceTotal;
+
+    /**
+     * @ORM\Column(type="decimal", precision=6, scale=2, nullable=true)
+     */
+    private $priceTotalSansTva;
+
+    /**
+     * @ORM\Column(type="string", length=50, nullable=true)
+     */
+    private $paymentType;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDateLessonOrder(): ?\DateTimeInterface
+    {
+        return $this->dateLessonOrder;
+    }
+
+    public function setDateLessonOrder(\DateTimeInterface $dateLessonOrder): self
+    {
+        $this->dateLessonOrder = $dateLessonOrder;
+
+        return $this;
+    }
+
+    public function getPriceTotal(): ?string
+    {
+        return $this->priceTotal;
+    }
+
+    public function setPriceTotal(string $priceTotal): self
+    {
+        $this->priceTotal = $priceTotal;
+
+        return $this;
+    }
+
+    public function getPriceTotalSansTva(): ?string
+    {
+        return $this->priceTotalSansTva;
+    }
+
+    public function setPriceTotalSansTva(?string $priceTotalSansTva): self
+    {
+        $this->priceTotalSansTva = $priceTotalSansTva;
+
+        return $this;
+    }
+
+    public function getPaymentType(): ?string
+    {
+        return $this->paymentType;
+    }
+
+    public function setPaymentType(?string $paymentType): self
+    {
+        $this->paymentType = $paymentType;
+
+        return $this;
+    }
+}
---
+++ src/Repository/LessonOrderRepository.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\LessonOrder;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Common\Persistence\ManagerRegistry;
+
+/**
+ * @method LessonOrder|null find($id, $lockMode = null, $lockVersion = null)
+ * @method LessonOrder|null findOneBy(array $criteria, array $orderBy = null)
+ * @method LessonOrder[]    findAll()
+ * @method LessonOrder[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class LessonOrderRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, LessonOrder::class);
+    }
+
+    // /**
+    //  * @return LessonOrder[] Returns an array of LessonOrder objects
+    //  */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('l')
+            ->andWhere('l.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('l.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?LessonOrder
+    {
+        return $this->createQueryBuilder('l')
+            ->andWhere('l.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
