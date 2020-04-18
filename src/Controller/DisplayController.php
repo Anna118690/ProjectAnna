77b29956a4eb8968c83b0ae6391c2e7acc698592
AnnaLaptop <anna.laskowska@hotmail.com>
2020-04-18 18:53:49 +0200

    controller display level language approach

--- src/Controller/DisplayController.php
+++ src/Controller/DisplayController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Level;
 use App\Entity\Course;
+use App\Entity\Approach;
+use App\Entity\Language;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -15,9 +18,18 @@ class DisplayController extends AbstractController
     {
         
             $entityManager = $this->getDoctrine()->getManager();
-            $rep = $entityManager->getRepository(Course::class);     
+            $rep = $entityManager->getRepository(Course::class);    
+            $rep2= $entityManager->getRepository(Level::class);
+            $rep3= $entityManager->getRepository(Language::class);
+            $rep4= $entityManager->getRepository(Approach::class);
+
+
             $courses = $rep->findAll();
-            $vars = ['courses' => $courses]; 
+            $levels = $rep2->findAll();
+            $languages = $rep3->findAll();
+            $approachs = $rep4->findAll();
+
+            $vars = ['courses' => $courses, 'levels' => $levels, 'languages'=>$languages, 'approachs'=>$approachs];
             return $this->render("display/courses.html.twig",$vars);
     }
 }
